7892e3e1a113597c1905ca1bebed00cbd34521a3
Justin Mendez <jmmendez@cpp.edu>
2022-12-14 15:26:54 -0800

    Remove TODO and unnecessary keyword

--- src/com/company/Visitor/Visitable.java
+++ src/com/company/Visitor/Visitable.java
@@ -1,8 +1,7 @@
 package com.company.Visitor;
 
 /*Visitor pattern component*/
-//TODO: implement this interface for both the User and Group classes
 public interface Visitable {
 
-    public void accept(EntryVisitor visitor);
+    void accept(EntryVisitor visitor);
 }
